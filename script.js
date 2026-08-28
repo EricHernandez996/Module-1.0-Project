a787b479b16926d0a0d4373bf7ac48ec9eb91bbe
EricHernandez996 <ericdhjr@gmail.com>
2026-08-28 11:59:57 -0500

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Question for my instructor: What is your favorite programming language?
+
+console.log("Hello! It's been a while since I've made one of these.");
+
+document.write("This message was created using JavaScript and document.write.");
